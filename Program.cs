80c77b65f7990f9bba71c3ab56dd2c8f0bab5d29
kaybh5 <129979442+kaybh5@users.noreply.github.com>
2023-04-10 16:08:12 -0400

    Hola como estan

--- Program.cs
+++ Program.cs
@@ -10,11 +10,11 @@ public class LoadTxt
 
     public LoadTxt(string direccion){ // metodo para cargar los txt :), constructor 
         Direccion = direccion;  //Esto almacena la direccion de la carpeta donde estan los archivos ejemplo: kayla/moogle/content
-        AllFilesPaths = Directory.GetFiles(this.Direccion);
-        TxtFilesPaths = new string[AllFilesPaths.Length];       
+        AllFilesPaths = Directory.GetFiles(this.Direccion); //esta variable contiene todas las direcciones de los archivos de la carpeta
+        TxtFilesPaths = new string[AllFilesPaths.Length]; // contiene solamente las direcciones de los archivos que terminen en .txt
     } 
 
-    public string[] CleanPaths(){
+    public string[] CleanPaths(){ // esta funcion va a almacenar en un nuevo array (this.TxtFilesPaths) las direcciones de archivos txt
         for (int i = 0; i < AllFilesPaths.Length; i++)
         {
             if (this.AllFilesPaths[i].Contains(".txt"))
@@ -29,12 +29,13 @@ public class LoadTxt
 
 
 
-class Program
+class Program // para probar 
 {
     static void Main(){
         LoadTxt Objeto1 = new LoadTxt("/Users/mariasilvia/Documents/Moogle/MoogleTest/content");
         Objeto1.CleanPaths();
         
+        //OOOooooooolllllllalalallalalalallal
         
         //System.Console.WriteLine(string.Join("\n ", Objeto1.Direcciones));
     }
