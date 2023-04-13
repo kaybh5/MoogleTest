d88a8c1d808726625fa082119321897848dfd2c4
Daniel Quesada <88943207+dnielpy@users.noreply.github.com>
2023-04-12 20:35:49 -0500

    Te agregue una cosa en la linea 49 que te va a imrpimir el array de palabras

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ public class LoadTxt
     public string[] TxtFilesPaths { get; set; }
 
 
-
     public LoadTxt(string direccion)
     { // metodo para cargar los txt :), constructor 
         Direccion = direccion;  //Esto almacena la direccion de la carpeta donde estan los archivos ejemplo: kayla/moogle/content
@@ -39,24 +38,14 @@ public class LoadTxt
 
 
     }
-
 }
-
-
-
-
 
 class Program // para probar 
 {
-
-
     static void Main()
     {
         LoadTxt Objeto1 = new LoadTxt("/Users/mariasilvia/Documents/Moogle/MoogleTest/content");
         Objeto1.CleanPaths();
-    
-        //System.Console.WriteLine(string.Join("\n ", Objeto1.Direcciones));
+        System.Console.WriteLine(string.Join(", ", Objeto1.AllWords()));    //Esto te va a imprimir el array de palabras :)
     }
-
-
 }
